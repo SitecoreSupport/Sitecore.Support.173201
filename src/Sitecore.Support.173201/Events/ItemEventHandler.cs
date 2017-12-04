ced550e2bac54c8cb3bcdba2bbf71a70fb3d45d2
Alen Pelin <alenpelin@gmail.com>
2017-12-04 15:19:43 +1100

    Fix cloning published item (closes #3)

---
+++ src/Sitecore.Support.173201/Events/ItemEventHandler.cs
@@ -0,0 +1,31 @@
+﻿namespace Sitecore.Support.Events
+{
+  using System;
+
+  using Sitecore.Diagnostics;
+  using Sitecore.Events;
+  using Sitecore.SecurityModel;
+  using Sitecore.Support.Publishing.Pipelines.PublishItem;
+
+  public class ItemEventHandler
+  {
+    public void OnItemCloneAdded([CanBeNull] object o, [NotNull] EventArgs e)
+    {
+      Assert.ArgumentNotNull(e, nameof(e));
+
+      using (new SecurityDisabler())
+      {
+        var item = SitecoreEventArgs.GetItem(e, 0);
+
+        if (string.IsNullOrEmpty(item[UpdateStatistics.FirstPublishedFieldID]))
+        {
+          return;
+        }
+
+        item.Editing.BeginEdit();
+        item[UpdateStatistics.FirstPublishedFieldID] = "";
+        item.Editing.EndEdit();
+      }
+    }
+  }
+}
